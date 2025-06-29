2ca5807d615770770d4e54f30a1c80566e5f533c
reddi122 <kancharlareddaiah122@gmail.com>
2025-06-29 18:44:50 +0530

    Update server.js

--- app2/server.js
+++ app2/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is Application 2");
+  res.send("Hello, this is Fashion Page");
 });
 
 app.listen(port, () => {
